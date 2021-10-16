c5a0fe9ea5e0e050d0e9ce1fa1af56685008b13e
Logan Willis <loganw0315@gmail.com>
2021-10-15 19:10:43 -0500

    Final version of password validator

--- password.js
+++ password.js
@@ -1,3 +1,13 @@
+console.log(` /$$$$$$$                                                                      /$$       /$$    /$$          /$$ /$$       /$$             /$$                        
+| $$__  $$                                                                    | $$      | $$   | $$         | $$|__/      | $$            | $$                        
+| $$  \\ $$ /$$$$$$   /$$$$$$$ /$$$$$$$ /$$  /$$  /$$  /$$$$$$   /$$$$$$   /$$$$$$$      | $$   | $$ /$$$$$$ | $$ /$$  /$$$$$$$  /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$ 
+| $$$$$$$/|____  $$ /$$_____//$$_____/| $$ | $$ | $$ /$$__  $$ /$$__  $$ /$$__  $$      |  $$ / $$/|____  $$| $$| $$ /$$__  $$ |____  $$|_  $$_/   /$$__  $$ /$$__  $$
+| $$____/  /$$$$$$$|  $$$$$$|  $$$$$$ | $$ | $$ | $$| $$  \\ $$| $$  \\__/| $$  | $$       \\  $$ $$/  /$$$$$$$| $$| $$| $$  | $$  /$$$$$$$  | $$    | $$  \\ $$| $$  \\__/
+| $$      /$$__  $$ \\____  $$\\____  $$| $$ | $$ | $$| $$  | $$| $$      | $$  | $$        \\  $$$/  /$$__  $$| $$| $$| $$  | $$ /$$__  $$  | $$ /$$| $$  | $$| $$      
+| $$     |  $$$$$$$ /$$$$$$$//$$$$$$$/|  $$$$$/$$$$/|  $$$$$$/| $$      |  $$$$$$$         \\  $/  |  $$$$$$$| $$| $$|  $$$$$$$|  $$$$$$$  |  $$$$/|  $$$$$$/| $$      
+|__/      \\_______/|_______/|_______/  \\_____/\\___/  \\______/ |__/       \\_______/          \\_/    \\_______/|__/|__/ \\_______/ \\_______/   \\___/   \\______/ |__/      
+
+`);
 console.log("Welcome to the password validator tool!");
 
 const readline = require('readline');
@@ -7,12 +17,12 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-reader.question("Enter password for validation: ", function(input){
+reader.question("Enter password for validation, must be at least 10 characters and contain numbers and letters: ", function(input){
     password = input
-    if (password.length >= 10){
+    if (password.length >= 10 && password.match(/\d+/g) && password.match(/[a-zA-Z]/g)){
         console.log('Password validation successful!');
     } else {
         console.log('Password validation failed!');
     }
-    reader.close()
+    reader.close();
 });
